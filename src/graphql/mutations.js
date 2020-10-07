b91471d0db0f4efe175e5eec97757149d468d5dd
Bryce Parkman <brycecparkman@gmail.com>
2020-10-07 17:36:21 -0400

    Added graph sql

---
+++ src/graphql/mutations.js
@@ -0,0 +1,27 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createTestmodel = /* GraphQL */ `
+  mutation CreateTestmodel($input: CreateTestmodelInput!) {
+    createTestmodel(input: $input) {
+      id
+      title
+    }
+  }
+`;
+export const deleteTestmodel = /* GraphQL */ `
+  mutation DeleteTestmodel($input: DeleteTestmodelInput!) {
+    deleteTestmodel(input: $input) {
+      id
+      title
+    }
+  }
+`;
+export const updateTestmodel = /* GraphQL */ `
+  mutation UpdateTestmodel($input: UpdateTestmodelInput!) {
+    updateTestmodel(input: $input) {
+      id
+      title
+    }
+  }
+`;
