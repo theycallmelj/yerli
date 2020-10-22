16d0518d0f352566151d36e5ffddc557993f4579
Shashank Lal <shashanklal01@gmail.com>
2020-10-22 23:41:16 +0400

    added logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,21 +5,31 @@ import Nav from 'react-bootstrap/Nav'
 import FormControl from 'react-bootstrap/FormControl'
 import Button from 'react-bootstrap/Button'
 
-export default function NavbarComponent(){
+export default function NavbarComponent() {
     return (
+
         <Navbar bg="light" expand="lg" fixed="top">
-        <Navbar.Brand href="/">Yerli</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-            <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-success">Search</Button>
-            </Form>
-            <Nav className="ml-auto">
-            <Nav.Link href="/">Feed</Nav.Link>
-            <Nav.Link href="/map">Map</Nav.Link>
-            </Nav>
-        </Navbar.Collapse>
+            <Navbar.Brand href="/" >
+                <img
+                    src="../images/logo.png"
+                    width="30"
+                    height="30"
+                    alt="Yerli"
+                    className="d-inline-block align-top"
+                >
+                </img>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Form inline>
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                    <Button variant="outline-success">Search</Button>
+                </Form>
+                <Nav className="ml-auto">
+                    <Nav.Link href="/">Feed</Nav.Link>
+                    <Nav.Link href="/map">Map</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
         </Navbar>
     )
 }
