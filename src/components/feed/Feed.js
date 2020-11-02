1bc61df599f1d98b49b349acd147305e1cbfcc1f
jack <jackbisceglia2000@gmail.com>
2020-11-01 22:45:51 -0500

    Added titles and subtitle to slider, Stacked slider in feed, moved mock data up the state tree

--- src/components/feed/Feed.js
+++ src/components/feed/Feed.js
@@ -1,10 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Slider from './Slider'
 
 export default function Feed(){
+    let [sliders, setSliders] = useState([
+    [
+        {
+            name: "Olive Garden",
+            picture: "https://brooklynreporter.com/wp-content/uploads/2019/04/Copy-of-Olive_Garden_DSC_1032-1-1024x683.jpg"
+        },
+        {
+            name: "Applebee's",
+            picture: "https://www.applebees.com/-/media/applebees/campaigns-2/health-and-safety/sanitation-lower-module-desktop.jpg?la=en&hash=87F93C0DB6C15FF647D5C4BE3A8D9F5938E36C9E"
+        },
+        {
+            name: "Chili's",
+            picture: "https://a57.foxnews.com/static.foxbusiness.com/foxbusiness.com/content/uploads/2020/08/0/0/Chilis.jpg?ve=1&tl=1"
+        },
+        {
+            name: "Chipotle",
+            picture: "https://130e178e8f8ba617604b-8aedd782b7d22cfe0d1146da69a52436.ssl.cf1.rackcdn.com/chipotle-hackers-dined-out-on-most-restaurants-showcase_image-8-a-9951.jpg"
+        }
+    ],
+    [
+        {
+            name: "McDonald's",
+            picture: "https://static01.nyt.com/images/2020/05/14/business/14mcdonalds-1/14mcdonalds-1-mobileMasterAt3x.jpg"
+        },
+        {
+            name: "Wendy's",
+            picture: "https://www.meatpoultry.com/ext/resources/MPImages/11-2018/11082018/wendys-exterior.jpg?t=1541774773&width=1080"
+        },
+        {
+            name: "Chik-Fil-A",
+            picture: "https://thechickenwire.chick-fil-a.com/-/media/amazon-cloudfront/images/cfacom/stories-images/press/finaloption3.ashx"
+        },
+        {
+            name: "Burger King",
+            picture: "https://dynaimage.cdn.cnn.com/cnn/c_fill,g_auto,w_1200,h_675,ar_16:9/https%3A%2F%2Fcdn.cnn.com%2Fcnnnext%2Fdam%2Fassets%2F200902125558-02-burger-king-new-restaurant-design---rendering.jpg"
+        }
+    ]
+    ]);
+
     return (
         <>
-        <Slider />
+        {
+            sliders.map(curr => (
+                <Slider businessArr={curr}/>
+            ))
+        }
         </>
     )
 }
--- src/components/feed/Slider.js
+++ src/components/feed/Slider.js
@@ -2,29 +2,24 @@ import React, {useState} from 'react'
 import BusinessCard from './BusinessCard.js'
 import './feed.css'
 
-export default function Slider() {
-    let [businesses, setBusinesses] = useState([
-        {
-            name: "Olive Garden",
-            picture: "https://brooklynreporter.com/wp-content/uploads/2019/04/Copy-of-Olive_Garden_DSC_1032-1-1024x683.jpg"
-        },
-        {
-            name: "Applebee's",
-            picture: "https://www.applebees.com/-/media/applebees/campaigns-2/health-and-safety/sanitation-lower-module-desktop.jpg?la=en&hash=87F93C0DB6C15FF647D5C4BE3A8D9F5938E36C9E"
-        },
-        {
-            name: "Chili's",
-            picture: "https://a57.foxnews.com/static.foxbusiness.com/foxbusiness.com/content/uploads/2020/08/0/0/Chilis.jpg?ve=1&tl=1"
-        }
-    ]);
+export default function Slider({businessArr}) {
+    let [businesses, setBusinesses] = useState(businessArr);
 
     return (
-        <div className="slider-wrapper" style={{height : "auto", width : "auto", display : "flex"}}>
-            {businesses.map((curr) => (
-                <BusinessCard businessTitle={curr.name} backgroundPicture={curr.picture}/>
-            ))}
-            <div className="nextSlide">
+        // Map Cards to Businesses
+        // Add
+        <div className="wrap"> 
+            <div className="titles">
+                <h3>Chain Restaurants</h3>
+                <h5>Big name restaurants in your area</h5>
+            </div>
+            <div className="slider-wrapper">
+                {businesses.map((curr) => (
+                    <BusinessCard businessTitle={curr.name} backgroundPicture={curr.picture}/>
+                ))}
+                <div className="nextSlide">
 
+                </div>
             </div>
         </div>
     )
