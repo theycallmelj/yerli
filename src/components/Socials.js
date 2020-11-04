e2adfa3d9f7c16c6c6d20ac77cfa31e29364f13c
Bryce Parkman <brycecparkman@gmail.com>
2020-11-03 19:47:31 -0500

    Added map feed and updated bottom socials bar

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -4,15 +4,23 @@ import Form from 'react-bootstrap/Form'
 import Nav from 'react-bootstrap/Nav'
 import FormControl from 'react-bootstrap/FormControl'
 import Button from 'react-bootstrap/Button'
+import { GrInstagram, GrFacebook } from 'react-icons/gr'
 
 export default function Socials(){
     return (
-        <Navbar bg="light" expand="lg" fixed="bottom">
+        <Navbar bg="light">
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-            <Nav>
-            <Nav.Link href="#">Instagram</Nav.Link>
-            <Nav.Link href="#">Facebook</Nav.Link>
+            <Nav className='mx-auto'>
+            <Nav.Link href="#">
+                <GrInstagram size="20" color='black'/>
+            </Nav.Link>
+            <Nav.Link href="#">
+                <GrFacebook size="20" color='black'/>
+            </Nav.Link>
+            <Nav.Link href="#" style={{color: 'black'}}>
+                <u>Suggest a Business</u>
+            </Nav.Link>
             </Nav>
         </Navbar.Collapse>
         </Navbar>
